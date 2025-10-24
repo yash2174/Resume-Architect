01dc0ecae9d836803b86dc0da56a8cc3759b217a
yash2174 <yashpaithane2004@gmail.com>
2025-10-24 06:20:49 -0700

    url change

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,11 @@ if (!process.env.MONGO_URI || !process.env.JWT_SECRET) {
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({
+    origin: 'https://resume-architect-3.onrender.com/api', // replace with actual frontend URL
+    credentials: true
+}));
+
 // --- Middleware ---
 app.use(cors());
 app.use(express.json({ limit: '10mb' })); // Increase payload limit for images in resume
